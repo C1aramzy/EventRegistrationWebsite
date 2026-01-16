725a5e679374656ad1bb90c4645e698d4d92aa3f
Natalie <nataliechen575@gmail.com>
2026-01-16 22:41:27 +0800

    Added Attendee Registeration

--- EventRegistrationWebsite/Models/Attendee.cs
+++ EventRegistrationWebsite/Models/Attendee.cs
@@ -6,15 +6,25 @@ namespace EventRegistrationWebsite.Models
     {
         public int AttendeeID { get; set; }
 
+        [Required]
+        [StringLength(50)]
         public string FirstName { get; set; } = string.Empty;
 
+        [Required]
+        [StringLength(50)]
         public string LastName { get; set; } = string.Empty;
 
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
 
+        [Phone]
+        [StringLength(30)]
         public string? PhoneNumber { get; set; }
 
+        [Required]
         public int EventID { get; set; }
+
         public Event? Event { get; set; }
     }
 }
